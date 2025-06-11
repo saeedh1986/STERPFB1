4dcdff182b3acb9e417f3d728634df319f92280e
Saeed Alhebsi <SaeedH1986@gmail.com>
2025-06-11 11:04:31 +0000

    I see this error with the app, reported by NextJS, please fix it. The er

--- src/lib/data.ts
+++ src/lib/data.ts
@@ -89,7 +89,7 @@ export const getColumns = (slug: string): ColumnDefinition[] => {
         else colorClass = 'text-gray-600 bg-gray-100';
         
         return <span className={`px-2 py-1 rounded-full text-xs font-medium ${colorClass}`}>{status}</span>;
-       }
+       };
     }
   });
   
