0a3cbad0f806a74e988f3e1975336331bca8fee8
Saeed Alhebsi <SaeedH1986@gmail.com>
2025-07-15 09:20:53 +0000

    dobble logi size and title

--- src/components/SidebarNav.tsx
+++ src/components/SidebarNav.tsx
@@ -74,8 +74,8 @@ export function SidebarNav() {
     <div className="flex h-full flex-col bg-sidebar text-sidebar-foreground glass-sidebar">
       <div className="flex h-20 items-center justify-center border-b border-sidebar-border px-4">
         <Link href="/dashboard" className="flex items-center gap-2 font-semibold">
-           {profile.logo && <Image src={profile.logo} alt="Company Logo" width={40} height={40} className="object-contain" />}
-          <span className="text-lg font-headline">{profile.erpName}</span>
+           {profile.logo && <Image src={profile.logo} alt="Company Logo" width={80} height={80} className="object-contain" />}
+          <span className="text-xl font-headline">{profile.erpName}</span>
         </Link>
       </div>
       <ScrollArea className="flex-1">
